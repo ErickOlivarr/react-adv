e0d7dee73ef714cef157cba2038a40ce55b3c4a3
Erick Olivar <erick.salvador.olivar@gmail.com>
2023-06-30 18:42:06 -0600

    lazyload basico listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,38 +5,57 @@ import {
   Navigate,
   NavLink,
 } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+//import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { routes } from './routes';
+
+import { Suspense } from 'react';
 
 
 const logo = '/vite.svg'; //esto hace referencia a la carpeta public en la raiz del proyecto
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } width={ 200 } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/lazy1" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 3</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <Suspense fallback={ <span>Loading...</span> }>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } width={ 200 } alt="React Logo" />
+            <ul>
+              {/* <li>
+                <NavLink to="/lazy1" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 1</NavLink>
+              </li>
+              <li>
+                <NavLink to="/lazy2" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 2</NavLink>
+              </li>
+              <li>
+                <NavLink to="/lazy3" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Lazy 3</NavLink>
+              </li> */}
 
-        <Routes>  {/* este elemento de Routes se toma en cuenta para el display:flex que tiene la clase main-layout definida en el index.css y que se le puso al div de arriba el cual contiene como elementos hijos el nav de arriba y este elemento Routes */}
-          <Route path="/lazy1" element={ <LazyPage1 /> } />
-          <Route path="/lazy2" element={ <LazyPage2 /> } />
-          <Route path="/lazy3" element={ <LazyPage3 /> } />
-          <Route path="/*" element={ <Navigate to="/lazy1" replace /> } />
-        </Routes>
+              {
+                routes.map(route => (
+                  <li key={ route.name }>
+                    <NavLink to={ route.to } className={ ({ isActive }) => isActive ? 'nav-active' : '' }>{ route.name }</NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
 
-      </div>
-    </BrowserRouter>
+          <Routes>  {/* este elemento de Routes se toma en cuenta para el display:flex que tiene la clase main-layout definida en el index.css y que se le puso al div de arriba el cual contiene como elementos hijos el nav de arriba y este elemento Routes */}
+            {/* <Route path="/lazy1" element={ <LazyPage1 /> } /> */}
+            {
+              routes.map(route => (
+                <Route key={ route.name } path={ route.path } element={ <route.Component /> } />
+              ))
+            }
+
+            <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
+          </Routes>
+
+        </div>
+      </BrowserRouter>
+    </Suspense>
+    
   );
 }
