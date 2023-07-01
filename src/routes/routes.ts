10a476e47040a07b8668819e6266457ea10641ef
Erick Olivar <erick.salvador.olivar@gmail.com>
2023-06-30 19:31:26 -0600

    rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 import { LazyExoticComponent, lazy } from 'react';
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 type JSXComponent = () => JSX.Element;
 
@@ -13,6 +14,7 @@ interface Route {
     name: string;
 };
 
+/*
 const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
 const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
 const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
@@ -38,5 +40,24 @@ export const routes: Route[] = [
         //Component: LazyPage3,
         Component: Lazy3,
         name: 'Lazy-3'
+    },
+];
+*/
+
+const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'));
+
+
+export const routes: Route[] = [
+    {
+        to: '/lazyload/',
+        path: '/lazyload/*',
+        Component: LazyLayout,
+        name: 'Lazy-1'
+    },
+    {
+        to: '/no-lazy',
+        path: '/no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
     },
 ];
